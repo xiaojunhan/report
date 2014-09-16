433f5465de472dac1eeac40ce7d65c003c3bf3f0
hanxiaojun <hanxiaojun85@gmail.com>
2014-09-16 17:25:49 +0800

    init

---
+++ yougeReport/src/com/youge/report/service/ReportService.java
@@ -0,0 +1,8 @@
+package com.youge.report.service;
+
+import java.util.List;
+
+public abstract class ReportService {
+
+	public abstract List getList();
+}
---
+++ yougeReport/src/com/youge/report/servlet/ReportServlet.java
@@ -0,0 +1,24 @@
+package com.youge.report.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ReportServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2322888475138310285L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String report = req.getParameter("report");
+		req.getRequestDispatcher("/WEB-INF/jsp/report.jsp").forward(req, resp);
+	}
+
+}
