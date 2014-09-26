6e41ec738d400c731febc220843f4d5f7a831427
hanxiaojun <hanxiaojun85@gmail.com>
2014-09-26 11:12:35 +0800

    s

--- yougeReport/src/com/youge/report/service/ReportServiceFactory.java
+++ yougeReport/src/com/youge/report/service/ReportServiceFactory.java
@@ -3,6 +3,12 @@ package com.youge.report.service;
 import com.youge.report.myreport.Test;
 
 public class ReportServiceFactory {
+	/**
+	 * 报表少的话 switch处理
+	 * 多的话 在xml、properties文件配置 使用反射
+	 * @param report
+	 * @return
+	 */
 	public static ReportService getInstance(String report) {
 		switch (report) {
 		case "test":
